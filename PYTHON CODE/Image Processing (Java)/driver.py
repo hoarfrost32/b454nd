168799496d7712e75c699a53160161b5799cbb3a
hoarfrost32 <aditya.tejpaul@research.iiit.ac.in>
2024-12-02 11:16:28 +0530

    pipe lining

---
+++ PYTHON CODE/Image Processing (Java)/driver.py
@@ -0,0 +1,17 @@
+import sys
+import papermill as pm
+
+# Get the uploaded image path from command-line arguments
+input_image_path = sys.argv[1]
+
+# Run the Jupyter notebook with the input image path as a parameter
+input_notebook = "project.ipynb"
+output_notebook = "executed_project.ipynb"
+
+pm.execute_notebook(
+    input_path=input_notebook,
+    output_path=output_notebook,
+    parameters={"image_path": input_image_path}
+)
+
+print("Notebook executed successfully. Output image should be in output.png")
